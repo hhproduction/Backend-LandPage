73d3609e2728b39895b415f582aeae6418b88755
HugoHenry <quanghieusieubeo@gmail.com>
2021-06-25 20:37:38 +0700

    update db

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,13 +2,18 @@ const mysql = require('mysql');
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    debug: false
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 })
+if(!pool){
+    console.log("Database is not connect.");
+}
+else{
+    console.log("Database is connected successful.");
+}
 
 const logMySQLQuerry = (sql, params) => {
     console.log('sql:',
