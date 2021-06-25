69428a92d1e5cfff570814b7fc0170ec83bdcfbd
HugoHenry <quanghieusieubeo@gmail.com>
2021-06-25 20:45:41 +0700

    db update

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,11 +6,11 @@ const pool = mysql.createPool({
     connectTimeout: 60 * 60 * 1000,
     acquireTimeout: 60 * 60 * 1000,
     timeout: 60 * 60 * 1000,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 if (!pool) {
     console.log("Database is not connect.");
