80be2a26dfacbc635a60be0bcf157c6fb6d46aed
HugoHenry <quanghieusieubeo@gmail.com>
2021-06-25 19:17:16 +0700

    db update

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,7 +6,8 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    debug: false
 })
 
 const logMySQLQuerry = (sql, params) => {
