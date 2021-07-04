7154d9b64dfb4faf7986c8f37faf76fed6bf87c1
HugoHenry <quanghieusieubeo@gmail.com>
2021-07-04 09:52:13 +0700

    discount

--- src/services/orderDetail.js
+++ src/services/orderDetail.js
@@ -32,19 +32,44 @@ const getOrderDetailbyId = async (id) => {
     }
 }
 
-const createOrderDetail = async ({orderId, productId, quantity, note}) => {
+const createOrderDetail = async ({ orderId, productId, quantity, note }) => {
     const sqlGetPrice = `
     select price
     from db_product
     where trash = 0 and id = ?;
     `
+    const sqlCheck = `
+    select count(id) as total
+    from db_discount
+    `
+    const sqlCheckDiscount = `
+    select discount_condition, percent
+    from db_discount
+    where productid = ?
+    `
     const sql = `
     insert into db_orderdetail(id, orderid, productid, quantity, price, note)
     values(uuid(),?,?,?,?,?)
     `
-    const {price} = await db.queryOne(sqlGetPrice,[productId])
-    const totalPrice = price*quantity
-    await db.query(sql, [orderId, productId, quantity,totalPrice, note])
+    const { total } = await db.queryOne(sqlCheck)
+    if (total == 0) {
+        const { price } = await db.queryOne(sqlGetPrice, [productId])
+        const totalPrice = price * quantity
+        await db.query(sql, [orderId, productId, quantity, totalPrice, note])
+    } else if (total > 0) {
+        const { discount_condition, percent } = await db.queryOne(sqlCheckDiscount, [productId])
+        if (quantity >= discount_condition) {
+            const { price } = await db.queryOne(sqlGetPrice, [productId])
+            const totalPrice = price * quantity * (1 - percent / 100)
+            await db.query(sql, [orderId, productId, quantity, totalPrice, note])
+        }
+        else {
+            const { price } = await db.queryOne(sqlGetPrice, [productId])
+            const totalPrice = price * quantity
+            await db.query(sql, [orderId, productId, quantity, totalPrice, note])
+        }
+    }
+
 }
 const updateOrderDetail = async ({ quantity, note }, id) => {
     const sqlGetProductId = `
@@ -52,13 +77,13 @@ const updateOrderDetail = async ({ quantity, note }, id) => {
     from db_orderdetail
     where trash = 0 and id = ?
     `
-    const {productId} = await db.queryOne(sqlGetProductId,[id])
+    const { productId } = await db.queryOne(sqlGetProductId, [id])
     const sqlGetPrice = `
     select price
     from db_product
     where trash = 0 and id = ?;
     `
-    const {price} = await db.queryOne(sqlGetPrice,[productId]).price*quantity
+    const { price } = await db.queryOne(sqlGetPrice, [productId]).price * quantity
     const sql = `
     update db_orderdetail
     set quantity=?,
