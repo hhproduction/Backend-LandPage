7fd58aad6ae2d6474d2e9dca26d4dc76a2869cc2
HugoHenry <quanghieusieubeo@gmail.com>
2021-07-10 01:45:48 +0700

    lets go

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -7,6 +7,7 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ID,
     secretAccessKey: process.env.AWS_SECRET,
     bucket: process.env.AWS_BUCKET_NAME,
+    apiVersion:env.AWS_API_VERSION
 })
 
 
