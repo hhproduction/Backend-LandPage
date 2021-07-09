b3d64bfabbabae7253aa85b0a301cd1453e57776
HugoHenry <quanghieusieubeo@gmail.com>
2021-07-10 02:57:26 +0700

    gogogogogogogog

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -6,7 +6,7 @@ const path = require('path')
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ID,
     secretAccessKey: process.env.AWS_SECRET,
-    bucket: process.env.AWS_BUCKET_NAME
+    bucket: "tradao-bucket"
 })
 
 
